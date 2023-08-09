e634d32d5d5df3326d58a4e08acee4eb2b2c335f
JohnNicol-AISim <141614150+JohnNicol-AISim@users.noreply.github.com>
2023-08-09 01:09:56 -0400

    Update MetaheuristicABO.py
    Corrected errors

--- MetaheuristicABO.py
+++ MetaheuristicABO.py
@@ -1,57 +1,76 @@
-import numpy as np
+import random
 
-# Define the objective function to be optimized
-def objective_function(x):
-    return (x - 2) ** 2
+# Objective function (Sphere function)
+def objective_function(position):
+    return sum(x**2 for x in position)
 
-# Artificial Bee Colony (ABC) algorithm
-def abc_optimization(num_bees, num_iterations, num_trials, bounds):
-    num_dimensions = len(bounds)
-    best_solution = None
-    best_fitness = float('inf')
+# Initialization function for the hive
+def initialize_hive(population_size, dimension, bounds):
+    return [[random.uniform(bounds[0], bounds[1]) for _ in range(dimension)] for _ in range(population_size)]
 
-    for _ in range(num_iterations):
-        # Employed bees phase
-        employed_bees = np.random.uniform(bounds[0], bounds[1], size=(num_bees, num_dimensions))
-        employed_fitness = [objective_function(x) for x in employed_bees]
+# Function to simulate employed bees
+def employed_bees(hive, trials, bounds):
+    for i in range(len(hive)):
+        new_bee = hive[i][:]
+        k = random.randint(0, len(hive) - 1)
+        j = random.randint(0, len(new_bee) - 1)
+        phi = random.uniform(-1, 1)
+        new_bee[j] = new_bee[j] + phi * (new_bee[j] - hive[k][j])
+        new_bee[j] = min(max(new_bee[j], bounds[0]), bounds[1])
 
-        # Onlooker bees phase
-        onlooker_probabilities = 1 / np.array(employed_fitness)
-        onlooker_probabilities /= np.sum(onlooker_probabilities)
+        if objective_function(new_bee) < objective_function(hive[i]):
+            hive[i] = new_bee
+            trials[i] = 0
+        else:
+            trials[i] += 1
 
-        for i in range(num_bees):
-            selected_bee = np.random.choice(range(num_bees), p=onlooker_probabilities)
-            trial_solution = employed_bees[i] + np.random.uniform(-1, 1, size=num_dimensions) * (employed_bees[i] - employed_bees[selected_bee])
-            trial_fitness = objective_function(trial_solution)
+# Function to simulate onlooker bees
+def onlooker_bees(hive, trials, bounds):
+    i = 0
+    t = 0
+    while t < len(hive):
+        if random.random() < (objective_function(hive[i]) / sum(objective_function(bee) for bee in hive)):
+            t += 1
+            new_bee = hive[i][:]
+            k = random.randint(0, len(hive) - 1)
+            j = random.randint(0, len(new_bee) - 1)
+            phi = random.uniform(-1, 1)
+            new_bee[j] = new_bee[j] + phi * (new_bee[j] - hive[k][j])
+            new_bee[j] = min(max(new_bee[j], bounds[0]), bounds[1])
 
-            if trial_fitness < employed_fitness[i]: # Correctly indented
-                employed_bees[i] = trial_solution
-                employed_fitness[i] = trial_fitness
+            if objective_function(new_bee) < objective_function(hive[i]):
+                hive[i] = new_bee
+                trials[i] = 0
+            else:
+                trials[i] += 1
+        i = (i + 1) % len(hive)
 
-        # Scout bees phase
-        best_bee_index = np.argmin(employed_fitness)
-        if employed_fitness[best_bee_index] < best_fitness:
-            best_solution = employed_bees[best_bee_index]
-            best_fitness = employed_fitness[best_bee_index]
+# Function to simulate scout bees
+def scout_bees(hive, trials, bounds, limit):
+    for i in range(len(trials)):
+        if trials[i] > limit:
+            hive[i] = [random.uniform(bounds[0], bounds[1]) for _ in range(len(hive[0]))]
+            trials[i] = 0
 
-        # Abandon solution if no improvement after a certain number of trials
-        trial_indices = np.where(employed_fitness >= best_fitness)
-        abandoned_indices = np.random.choice(trial_indices[0], size=num_trials, replace=False)
-        employed_bees[abandoned_indices] = np.random.uniform(bounds[0], bounds[1], size=(num_trials, num_dimensions))
+# Main function to perform optimization
+def abc_optimization(objective_function, bounds, dimension, population_size, max_generations, limit):
+    hive = initialize_hive(population_size, dimension, bounds)
+    trials = [0] * population_size
 
-    return best_solution, best_fitness
+    for generation in range(max_generations):
+        employed_bees(hive, trials, bounds)
+        onlooker_bees(hive, trials, bounds)
+        scout_bees(hive, trials, bounds, limit)
+        best_solution = min(hive, key=objective_function)
+        print(f"Generation {generation}: Best solution = {best_solution}, Value = {objective_function(best_solution)}")
 
-# Main function to run ABC optimization
-def main():
-    num_bees = 50
-    num_iterations = 100
-    num_trials = 5
-    bounds = (-10, 10)
+    return min(hive, key=objective_function)
 
-    best_solution, best_fitness = abc_optimization(num_bees, num_iterations, num_trials, bounds)
-
-    print("Best Solution:", best_solution)
-    print("Best Fitness:", best_fitness)
-
-if __name__ == "__main__":
-    main()
+# Example usage
+bounds = (-10, 10)
+dimension = 5
+population_size = 20
+max_generations = 100
+limit = 5
+best_solution = abc_optimization(objective_function, bounds, dimension, population_size, max_generations, limit)
+print(f"Final solution: {best_solution}, Value: {objective_function(best_solution)}")
