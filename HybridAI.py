d3bb64f78bf524b9db41841d879a7725a82bdf28
JohnNicol-AISim <141614150+JohnNicol-AISim@users.noreply.github.com>
2023-08-09 01:43:36 -0400

    Update HybridAI.py

--- HybridAI.py
+++ HybridAI.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Define the environment for the simulation (you can replace this with your own simulation)
+# Define the environment for the simulation
 class SimulationEnvironment:
     def __init__(self, num_agents, num_features):
         self.num_agents = num_agents
@@ -17,19 +17,19 @@ class SimulationEnvironment:
         self.state = np.random.randint(0, 2, (self.num_agents, self.num_features))  # Random state for the next step
         return self.state, rewards
 
-# Define the reinforcement learning agent (you can replace this with your own RL agent implementation)
+# Define the reinforcement learning agent
 class RLAgent:
     def __init__(self, num_actions, num_features):
         self.num_actions = num_actions
         self.num_features = num_features
         self.q_values = np.random.rand(num_features, num_actions)
 
-    def choose_action(self, state):
-        # Choose action based on the Q-values and the current state
-        action = np.argmax(np.dot(state, self.q_values))
-        return action
+    def choose_actions(self, state):
+        # Choose actions for all agents based on the Q-values and the current state
+        actions = np.argmax(np.dot(state, self.q_values), axis=1)
+        return actions
 
-# Define the genetic algorithm (you can replace this with your own GA implementation)
+# Define the genetic algorithm
 class GeneticAlgorithm:
     def __init__(self, num_agents, num_features, num_generations):
         self.num_agents = num_agents
@@ -63,14 +63,14 @@ if __name__ == "__main__":
     ga = GeneticAlgorithm(num_agents, num_features, num_generations)
 
     for episode in range(100):
-        # RL agent chooses action
+        # RL agent chooses actions for all agents
         state = environment.state
-        rl_action = rl_agent.choose_action(state)
+        rl_actions = rl_agent.choose_actions(state)
 
         # GA optimizes actions
         ga_actions = ga.optimize(environment)
 
-        # Take a step in the environment
-        state, rewards = environment.step(rl_action)
+        # Take a step in the environment with RL actions
+        state, rewards = environment.step(rl_actions)
 
-        print(f"Episode: {episode}, RL Action: {rl_action}, GA Actions: {ga_actions}, Rewards: {rewards}")
+        print(f"Episode: {episode}, RL Actions: {rl_actions}, GA Actions: {ga_actions}, Rewards: {rewards}")
